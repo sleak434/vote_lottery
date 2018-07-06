2b126c81ae2cdbdfdbfb23057e90826e7e1fa4bc
jungkook <2jungkook@users.noreply.github.com>
2018-07-06 19:32:25 +0900

    Update votelottery.sol

--- votelottery.sol
+++ votelottery.sol
@@ -6,11 +6,12 @@ import "./SafeMath.sol";
 contract votelottery is Ownable {
     using SafeMath for uint256;
     mapping(address => bool) public voters;
-    struct cadidate {
+    address [] public tickets;
+    struct candidate {
         string name;
         uint256 votes;
     }
-    cadidate[] public candidates;
+    candidate[] public candidates;
     
     bool public vote_started;
     bool public vote_is_over;
@@ -24,10 +25,9 @@ contract votelottery is Ownable {
         vote_started = false;
         vote_is_over = false;
     }
-    
-    event VoteResult(
-        string indexed name,
-        uint indexed votes
+    event VoteResult(	
+        string indexed name,	
+        uint indexed votes	
     );
     
     function renounceOwnership() public onlyOwner {
@@ -50,8 +50,7 @@ contract votelottery is Ownable {
         for (uint256 i = 0; i < length; i = i.add(1)) {
             require(keccak256(bytes(candidate_name)) != keccak256(bytes(candidates[i].name)));
         }
-        candidates.length = candidates.length.add(1);
-        candidates[length].name = candidate_name;
+        candidates.push(candidate(candidate_name, 0));
     }
     
    function addVoters(address[] new_voters) onlyOwner vote_not_over public {
@@ -60,6 +59,7 @@ contract votelottery is Ownable {
         for (uint256 i = 0; i < length; i = i.add(1)) {
             require(new_voters[i] != address(0));
             voters[new_voters[i]] = true;
+            tickets.push(new_voters[i]);
         }
     }
     
@@ -73,20 +73,19 @@ contract votelottery is Ownable {
         require(vote_started == true);
         vote_is_over = true;
         // call some functions
-        getResult();
     }
     
-    // 이미 투표를 했으면 투표할 수 없다.
     modifier canVote() {
         require(voters[msg.sender] == false);
+        require(vote_started == true);
         _;
     }
     
     // 보유하고 있는 투표권을 후보자(candidates[candidate])에 행사한다.
-    function vote(uint256 candidate) canVote vote_not_over public {
+    function vote(uint256 candidate_) canVote vote_not_over public {
         voters[msg.sender] = true;
         // 유효하지 않은 후보자일 경우 트랜잭션 전 상태로 돌아가게 예외처리 되있음
-        candidates[candidate].votes = candidates[candidate].votes.add(1);
+        candidates[candidate_].votes = candidates[candidate_].votes.add(1);
     }
     
     event winner(string _msg, address _address, uint _amount);
@@ -107,10 +106,10 @@ contract votelottery is Ownable {
         emit winner('winner is', _winner, _amount);
     }
     
-     function getResult() public {
-        
-        for(uint256 p = 0; p < candidates.length ; p++) {
-            emit VoteResult(candidates[p].name, candidates[p].votes);     
+    function getResult() public {	
+ 	
+        for(uint256 p = 0; p < candidates.length ; p++) {	
+            emit VoteResult(candidates[p].name, candidates[p].votes);     	
         }
     }
 }
